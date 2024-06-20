a84e7201a444c0d3eb32368e424ea5400cda0906
Joeyybad <jnkunga@hotmail.com>
2024-06-20 09:12:56 +0200

    middlewares change

--- api/middleware/protectSearch.js
+++ api/middleware/protectSearch.js
@@ -1,13 +1,18 @@
-const protectSearch = (req, res, next) => {
-    const searchQuery = req.body.search;
-    const regex = /^[^<>]*$/; 
+const { check, validationResult } = require('express-validator');
 
-    if (regex.test(searchQuery)) {
-        // Si la recherche ne contient pas les caractères < et > on tolère
-        next(); 
-    } else {
-        // Si la recherche contient les caractères < et >
-        res.status(400).send("Requête invalide : Caractères non autorisés.");
+const protectSearch = [
+    check('search')
+        .isLength({ max: 100 }).withMessage('Requête invalide : La recherche est trop longue.')
+        .matches(/^[a-zA-Z0-9\s-]*$/).withMessage('Requête invalide : Caractères non autorisés.')
+        .escape(), // Échappe les caractères potentiellement dangereux
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
     }
-};
+];
+
 module.exports = protectSearch;
